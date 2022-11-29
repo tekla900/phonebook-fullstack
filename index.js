5135d21b371bd167d765a2c6fa1fd2143724b7bd
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-11-29 21:12:23 +0400

    new phonebook entries can be added by making HTTP POST

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
 let persons = [
     { 
         "id": 1,
@@ -54,10 +56,34 @@ app.get('/api/persons/:id', (request, response) => {
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
-    console.log(`request.params.id type: ${typeof id}`)
-    console.log(`person id type: ${typeof persons.map(e => e.id)[0]}`)
-
     response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+}
+  
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({ 
+        error: 'name missing' 
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
