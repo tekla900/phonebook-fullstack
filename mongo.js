e422974a1c169790bbc3dfe18e9f50ffa70434fb
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-11-30 12:54:16 +0400

    Created a mongo.js file in the project directory, that can be used for adding entries to the phonebook, and for listing all of the existing entries in the phonebook

--- mongo.js
+++ mongo.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
-    process.exit(1)
-}
+    console.log('Please provide the password as an argument: node mongo.js <password>');
+    process.exit(1);
+} 
 
 const password = process.argv[2]
 
 const url = `mongodb+srv://fullstack:${password}@cluster0.jsfdfqg.mongodb.net/?retryWrites=true&w=majority`
+mongoose.connect(url)
+console.log('connected')
 
 const personSchema = new mongoose.Schema({
     name: String,
@@ -16,20 +18,24 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-mongoose
-    .connect(url)
-    .then((result) => {
-        console.log('connected')
-
-        const person = new Person({
-            name: 'tekla',
-            number: 123,
+if (process.argv.length == 3) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(person)
         })
+        mongoose.connection.close()
+    })
+} else {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
 
-        return person.save()
-    })
-    .then(() => {
-        console.log('person saved')
-        return mongoose.connection.close()
-    })
-    .catch((err) => console.log(err))
+    person
+        .save()
+        .then(() => {
+            console.log(`added ${name} number ${number} to phonebook`)
+            return mongoose.connection.close()
+        })
+        .catch((err) => console.log(err))
+}
