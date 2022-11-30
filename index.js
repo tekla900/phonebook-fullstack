918c76eb4c156e438f842fb5af17dd3606d6b6c8
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-11-30 11:10:56 +0400

    added cors to resolve same-origin-polici problem

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
 let morgan = require('morgan')
-
-app.use(express.json())
-
+const cors = require('cors')
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
+app.use(cors())
+app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 let persons = [
     { 
         "id": 1,
